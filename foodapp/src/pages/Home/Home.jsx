39504b2e8189025c7f93f988c366cfd0bd2859ce
Priyanshu-1124 <priyanshusom007@gmail.com>
2025-09-08 21:02:24 +0530

    feat (foodapp/home): add explore menu sections

--- foodapp/src/pages/Home/Home.jsx
+++ foodapp/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
 import Header from "../../components/Header/Header"
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All")
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
