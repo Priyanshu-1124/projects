259a8db7920dfe56ffff3e8eb6e5e3321cdbbd9c
Priyanshu-1124 <priyanshusom007@gmail.com>
2025-09-14 23:22:16 +0530

    feat (home/foodapp): Refactor cart item logic into Context API

--- foodapp/src/components/FoodItem/FoodItem.jsx
+++ foodapp/src/components/FoodItem/FoodItem.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import "./FoodItem.css"
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id, name, price, description, image}) => {
 
-  const [itemCount, setItemCount] = useState(0)
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext)
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
         {
-          !itemCount
-          ?<img src={assets.add_icon_white} className='add' onClick={()=>setItemCount(prev=>prev+1)}/>
+          !cartItems[id]
+          ?<img src={assets.add_icon_white} className='add' onClick={()=>addToCart(id)}/>
           :<div className="food-item-counter">
-            <img src={assets.remove_icon_red} onClick={()=>setItemCount(prev=>prev-1)}/>
-            <p>{itemCount}</p>
-            <img src={assets.add_icon_green} onClick={()=>setItemCount(prev=>prev+1)}/>
+            <img src={assets.remove_icon_red} onClick={()=>removeFromCart(id)}/>
+            <p>{cartItems[id]}</p>
+            <img src={assets.add_icon_green} onClick={()=>addToCart(id)}/>
           </div>
         }
       </div>
