66b2b9c859dd57c099bf3f48d1f62fea3445a0d7
Priyanshu-1124 <priyanshusom007@gmail.com>
2025-09-24 23:14:56 +0530

    feat(cart/foodapp): add cart functionality

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Cart from './pages/Cart/Cart'
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/cart" element={<Cart/>}/>
         </Routes>
       </div>
       <Footer/>
